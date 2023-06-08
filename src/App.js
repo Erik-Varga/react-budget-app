707639d6484a87561e37c471227f1b249ede77f5
Erik Varga <viglew@gmail.com>
2023-06-08 18:09:23 -0400

    configured BudgetCard

--- src/App.js
+++ src/App.js
@@ -12,17 +12,18 @@ function App() {
     <div className='card d-flex flex-column justify-content-center align-items-center gap-1 m-2 p-2 text-center'>
       <div className='container p-4 shadow-sm'>
         <h3 className='fw-bold'>Budgets</h3>
-        <div className='p-1'>
+        <div className='p-1 mb-2'>
           <Button className='btn btn-sm m-1 px-4' variant='primary'>Add Budget</Button>
           <Button className='btn btn-sm m-1 px-4' variant='outline-primary'>Add Expense</Button>
         </div>
+
           <div style={{
             // display: "grid",
             // gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
             // gap: "1rem",
             // alignItems: "flex-start"
           }}>
-          <BudgetCard name="Entertainment" amount={200} max={1000} />
+          <BudgetCard name="Entertainment" amount={200} max={1000} gray />
 
           </div>
       </div>
--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card, ProgressBar } from 'react-bootstrap';
 import { currencyFormatter } from '../utils';
 
-export default function BudgetCard({ name, amount, max }) {
+export default function BudgetCard({ name, amount, max, gray }) {
+  const classNames = []
+  if (amount > max) {
+    classNames.push('bg-danger', 'bg-opacity-10')
+  } else if (gray) {
+    classNames.push('bg-light')
+  }
+
+  function getProgressBarVariant(amount, max) {
+    const ratio = amount / max
+    if (ratio < 0.5) return 'primary'
+    if (ratio < 0.75) return 'warning'
+    return 'danger'
+  }
+
   return (
-    <Card>
+    <Card className={classNames.join(" ")}>
       <Card.Body>
-        <Card.Title className='d-flex flex-column justify-content-between'>
-            <div>{name}</div>
-            <div>{currencyFormatter.format(amount)} / {currencyFormatter.format(max)}</div>
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal">
+          <div className="me-2">{name}</div>
+          <div>
+            {currencyFormatter.format(amount)}
+            <span className="text-muted fs-6 ms-1">
+              / {currencyFormatter.format(max)}
+            </span>
+          </div>
         </Card.Title>
+        <ProgressBar variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount} />
+        <div className='p-1 m-3'>
+          <Button className='btn btn-sm m-1 px-4 bg-white' variant='outline-primary'>Add Expense</Button>
+          <Button className='btn btn-sm m-1 px-4 bg-white' variant='outline-secondary'>View Expenses</Button>
+        </div>
       </Card.Body>
     </Card>
-  )
+  );
 }
